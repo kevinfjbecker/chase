36fb127c41b88cdec41a554d357c399d0e5ffc0d
Kevin Becker <kevinfjbecker@gmail.com>
2025-02-17 01:32:48 +0100

    Debug UI

--- src/script.js
+++ src/script.js
@@ -1,34 +1,50 @@
 import * as d3 from 'd3'
+import GUI from 'lil-gui'
+
+const gui = new GUI()
+const debugObject = {
+    node: {
+        color: '#4682B4',
+        radius: 5
+    }
+}
+gui
+    .addColor(debugObject.node, 'color')
+    .onChange(() =>
+    {
+        node.attr('fill', debugObject.node.color)
+    })
+gui
+    .add(debugObject.node, 'radius')
+    .min(1)
+    .max(10)
+    .step(1)
+    .onChange(() =>
+    {
+        node.attr('r', debugObject.node.radius)
+    })
+
 
 const data = await d3.json('london.json');
 
-// Specify the dimensions of the chart.
-const width = 928;
-const height = 600;
+const width = 2400;
+const height = 2400;
 
-// Specify the color scale.
-const color = d3.scaleOrdinal(d3.schemeCategory10);
-
-// The force simulation mutates links and nodes, so create a copy
-// so that re-evaluating this cell produces the same result.
 const links = data.links.map(d => ({ ...d }));
 const nodes = data.nodes.map(d => ({ ...d }));
 
-// Create a simulation with several forces.
 const simulation = d3.forceSimulation(nodes)
     .force("link", d3.forceLink(links).id(d => d.id))
     .force("charge", d3.forceManyBody())
     .force("center", d3.forceCenter(width / 2, height / 2))
     .on("tick", ticked);
 
-// Create the SVG container.
 const svg = d3.select('body').append("svg")
     .attr("width", width)
     .attr("height", height)
     .attr("viewBox", [0, 0, width, height])
     .attr("style", "max-width: 100%; height: auto;");
 
-// Add a line for each link, and a circle for each node.
 const link = svg.append("g")
     .attr("stroke", "#999")
     .attr("stroke-opacity", 0.6)
@@ -43,19 +59,17 @@ const node = svg.append("g")
     .selectAll()
     .data(nodes)
     .join("circle")
-    .attr("r", 5)
-    .attr("fill", d => color(d.group));
+    .attr("r", debugObject.node.radius)
+    .attr("fill", debugObject.node.color);
 
 node.append("title")
     .text(d => d.id);
 
-// Add a drag behavior.
 node.call(d3.drag()
     .on("start", dragstarted)
     .on("drag", dragged)
     .on("end", dragended));
 
-// Set the position attributes of links and nodes each time the simulation ticks.
 function ticked() {
     link
         .attr("x1", d => d.source.x)
@@ -68,31 +82,17 @@ function ticked() {
         .attr("cy", d => d.y);
 }
 
-// Reheat the simulation when drag starts, and fix the subject position.
 function dragstarted(event) {
     if (!event.active) simulation.alphaTarget(0.3).restart();
     event.subject.fx = event.subject.x;
     event.subject.fy = event.subject.y;
 }
-
-// Update the subject (dragged node) position during drag.
 function dragged(event) {
     event.subject.fx = event.x;
     event.subject.fy = event.y;
 }
-
-// Restore the target alpha so the simulation cools after dragging ends.
-// Unfix the subject position now that it’s no longer being dragged.
 function dragended(event) {
     if (!event.active) simulation.alphaTarget(0);
     event.subject.fx = null;
     event.subject.fy = null;
 }
-
-// When this cell is re-run, stop the previous simulation. (This doesn’t
-// really matter since the target alpha is zero and the simulation will
-// stop naturally, but it’s a good practice.)
-// invalidation.then(() => simulation.stop());
-
-console.log(svg.node())
-
